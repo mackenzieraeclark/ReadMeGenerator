36bd09847f0290253467b3f7f6f755d88f303e06
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-17 16:11:47 -0400

    question function fix

--- index.js
+++ index.js
@@ -2,65 +2,67 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown")
 
-const questions = [
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "username"
-      },
-      {
-        type: "input",
-        message: "What is your email?",
-        name: "email"
-      },
-      {
-        type: "input",
-        message: "What is the GitHub URL of your application?",
-        name: "url"
-      },
-      {
-        type: "input",
-        message: "What is the title of your application?",
-        name: "title"
-      },
-      {
-        type: "input",
-        message: "Include a description of your application.",
-        name: "description"
-      },
-      {
-        type: "input",
-        message: "What packages need to be installed to run your application?",
-        name: "installation"
-      },
-      {
-        type: "input",
-        message: "What technologies are used in this application?",
-        name: "technology"
-      },
-      {
-        type: "input",
-        message: "Include an example of how your application is used.",
-        name: "usage"
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "What kind of license would you like to have?",
-        name: "license",
-        choices: ["MIT", "APACHE 2.0", "GPL v3", "BSD 3", "None"]
-      },
-      {
-        type: "input",
-        message: "Include all project contributors, if applicable.",
-        name: "contributer"
-      },
-      {
-        type: "input",
-        message: "What command is used to run a test?",
-        name: "tests",
-      }
-];
+function askQuestions() {
+    return inquirer.prompt( [
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username"
+          },
+          {
+            type: "input",
+            message: "What is your email?",
+            name: "email"
+          },
+          {
+            type: "input",
+            message: "What is the GitHub URL of your application?",
+            name: "url"
+          },
+          {
+            type: "input",
+            message: "What is the title of your application?",
+            name: "title"
+          },
+          {
+            type: "input",
+            message: "Include a description of your application.",
+            name: "description"
+          },
+          {
+            type: "input",
+            message: "What packages need to be installed to run your application?",
+            name: "installation"
+          },
+          {
+            type: "input",
+            message: "What technologies are used in this application?",
+            name: "technology"
+          },
+          {
+            type: "input",
+            message: "Include an example of how your application is used.",
+            name: "usage"
+          },
+          {
+            type: "list",
+            name: "license",
+            message: "What kind of license would you like to have?",
+            name: "license",
+            choices: ["MIT", "APACHE 2.0", "GPL v3", "BSD 3", "None"]
+          },
+          {
+            type: "input",
+            message: "Include all project contributors, if applicable.",
+            name: "contributer"
+          },
+          {
+            type: "input",
+            message: "What command is used to run a test?",
+            name: "tests",
+        }
+    ]);
+};
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, "utf8", function (err) {
@@ -75,7 +77,7 @@ async function init() {
     try {
         const answers = await askQuestions();
         generateMarkdown(answers);
-        writetoFile("README.md", generateMarkdown(answers));
+        writeToFile("README.md", generateMarkdown(answers));
     
         } catch (err) {
           console.log(err);
